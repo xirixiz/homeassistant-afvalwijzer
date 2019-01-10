9dcb4fb7c7889df49e322217a93c4329a11a21e3
Bram van Dartel <spam@rootrulez.com>
2019-01-10 11:28:04 +0100

    Update mijnafvalwijzer_scraper.py

--- custom_components/sensor/mijnafvalwijzer_scraper.py
+++ custom_components/sensor/mijnafvalwijzer_scraper.py
@@ -1,66 +1,96 @@
 import bs4
 import requests
+import itertools
 from datetime import datetime, timedelta, date
 
 def scraper(url, trash=None):
-    # today = datetime.today().strftime("%Y-%m-%d")
-    # dateConvert = datetime.strptime(today, "%Y-%m-%d") + timedelta(days=1)
-    # tomorrow = datetime.strftime(dateConvert, "%Y-%m-%d")
-
     if not trash:
-        uniqueTrashTypes = []
-        uniqueTrashNames = []
-        trashDates = []
+        trashDump = []
         trashSchedule = []
-        devices = []
+        uniqueTrashShortNames = []
+        uniqueTrashLongNames = []
 
     page = requests.get(url)
     soup = bs4.BeautifulSoup(page.text, "html.parser")
-    #soup = bs4.BeautifulSoup(page.text, "lxml")
 
     # Get trash date
     try:
-        for element in soup.select('a[href*="#waste"] p[class]'):
-            name = element.get_text()
-            trashDates.append(name)
+        for data in soup.select('a[href*="#waste"] p[class]'):
+            element = data["class"]
+            for item in element:
+                x = item
+            name = data.get_text()
+            trashDump.append(name)
+            trashDump.append(x)
     except IndexError:
         return 'No matching trashname(s) found.'
 
-    #trashDates = [i.split('\n', 1)[0] for i in uniqueTrashDates]
-    uniqueTrashDates = [i.split('\n', 1) for i in trashDates]
+    uniqueTrashDates = [i.split('\n', 1) for i in trashDump]
+    uniqueTrashDates = list(itertools.chain.from_iterable(uniqueTrashDates))
+    uniqueTrashDates = [uniqueTrashDates[i:i+3]for i in range(0,len(uniqueTrashDates),3)]
 
-    for item in uniqueTrashDates:
-        uniqueTrashMeuk = {}
-        uniqueTrashMeuk['key'] = item[1]
-        uniqueTrashMeuk['value'] = item[0]
-        trashSchedule.append(uniqueTrashMeuk)
+    try:
+        for item in uniqueTrashDates:
+            trashDump = {}
+            trashDump['key'] = item[2]
+            trashDump['description'] = item[1]
+            trashDump['value'] = item[0]
+            trashSchedule.append(trashDump)
+    except IndexError:
+        return 'No matching trashname(s) found.'
 
     print (trashSchedule)
 
-
     # Get trash shortname
     try:
-        for element in soup.select('a[href*="#waste"] p[class]'):
-            devices.extend(element["class"])
-        for element in devices:
-            if element not in uniqueTrashTypes:
-                uniqueTrashTypes.append(element)
-    except IndexError:
-        return 'No matching trashtype(s) found.'
-
-
-    # Get trash longname
-    try:
-        for element in soup.select('span[class="afvaldescr"]'):
-            name = element.get_text()
-            if name not in uniqueTrashNames:
-                uniqueTrashNames.append(name)
+        for item in trashSchedule:
+            element = item.get('key')
+            if element not in uniqueTrashShortNames:
+                uniqueTrashShortNames.append(element)
     except IndexError:
         return 'No matching trashname(s) found.'
 
-    print (uniqueTrashTypes)
-    print (uniqueTrashNames)
+    print (uniqueTrashShortNames)
+
+    # Get trash longname
+    try:
+        for item in trashSchedule:
+            element = item.get('description')
+            if element not in uniqueTrashLongNames:
+               uniqueTrashLongNames.append(element)
+    except IndexError:
+        return 'No matching trashname(s) found.'
+
+    print (uniqueTrashLongNames)
+
+
+
+    # ALTERNATIVES
+
+    # # Get trash shortname
+    # try:
+    #     for element in soup.select('a[href*="#waste"] p[class]'):
+    #         devices.extend(element["class"])
+    #     for element in devices:
+    #         if element not in uniqueTrashShortNames:
+    #             uniqueTrashShortNames.append(element)
+    # except IndexError:
+    #     return 'No matching trashtype(s) found.'
+
+    # #print (uniqueTrashShortNames)
+
+
+    # # Get trash longname
+    # try:
+    #     for element in soup.select('span[class="afvaldescr"]'):
+    #         name = element.get_text()
+    #         if name not in uniqueTrashLongNames:
+    #             uniqueTrashLongNames.append(name)
+    # except IndexError:
+    #     return 'No matching trashname(s) found.'
+
+    # #print (uniqueTrashLongNames)
 
 if __name__ == '__main__':
-    trash = scraper('https://www.mijnafvalwijzer.nl/nl/xxxxxx/x')
+    trash = scraper('https://www.mijnafvalwijzer.nl/nl/xxxx/x')
     #print(trash)
