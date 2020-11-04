70c56620c6e3c41e46817b82abc139333ec6ec1e
Bram van Dartel <root@rootrulez.com>
2020-11-04 11:08:52 +0100

    update date selector next

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -83,7 +83,7 @@ DOMAIN_DATA = "afvalwijzer_data"
 STARTUP_MESSAGE = """
 -------------------------------------------------------------------
 Afvalwijzer
-Version: 5.1.1
+Version: 5.1.2
 This is a custom integration!
 If you have any issues with this you need to open an issue here:
 "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
