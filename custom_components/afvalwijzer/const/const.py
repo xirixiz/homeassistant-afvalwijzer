f6aa7433fa28dea55c8492b827a1027485378860
Bram van Dartel <bram.van.dartel@asml.com>
2020-12-09 11:31:11 +0100

    migrate from datetime to date only

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -41,7 +41,7 @@ DOMAIN_DATA = "afvalwijzer_data"
 STARTUP_MESSAGE = """
 -------------------------------------------------------------------
 Afvalwijzer
-Version: 5.2.3
+Version: 5.2.4
 This is a custom integration!
 If you have any issues with this you need to open an issue here:
 "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
