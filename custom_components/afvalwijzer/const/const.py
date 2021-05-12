87061413e147b7debf54db27da0a925b8d1048cb
Bram van Dartel <spam@rootrulez.com>
2021-05-12 08:07:40 +0200

    Update const.py

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -45,10 +45,10 @@ DOMAIN_DATA = "afvalwijzer_data"
 
 STARTUP_MESSAGE = f"""
 -------------------------------------------------------------------
-{NAME}
-Version: {VERSION}
+Afvalwijzer
+Version: 2021.02.01
 This is a custom integration!
 If you have any issues with this you need to open an issue here:
-{ISSUE_URL}
+https://github.com/xirixiz/homeassistant-afvalwijzer/issues
 -------------------------------------------------------------------
 """
