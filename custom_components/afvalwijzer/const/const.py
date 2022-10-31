12f2b2449acb78aa56f86ee7529a0ef059791abc
Bram van Dartel <bram.van.dartel@outlook.com>
2022-10-31 09:14:54 +0100

    fix postal code upper case issue

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -5,7 +5,7 @@ _LOGGER = logging.getLogger(__name__)
 
 API = "api"
 NAME = "afvalwijzer"
-VERSION = "2022.11.02"
+VERSION = "2022.11.03"
 ISSUE_URL = "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
 
 SENSOR_COLLECTOR_TO_URL = {
