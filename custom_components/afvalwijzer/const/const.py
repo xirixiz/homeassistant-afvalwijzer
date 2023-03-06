212575b15e75109fa81f6b2c0e59fc5d39cad0a1
Bram van Dartel <root@rootrulez.com>
2023-03-06 11:24:43 +0100

    update

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -5,7 +5,7 @@ _LOGGER = logging.getLogger(__name__)
 
 API = "api"
 NAME = "afvalwijzer"
-VERSION = "2023.03.01"
+VERSION = "023.03.02"
 ISSUE_URL = "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
 
 SENSOR_COLLECTOR_TO_URL = {
