d84052d4eadcea6ab29842a44d9ebe6a80c8a868
Bram van Dartel <root@rootrulez.com>
2023-12-28 06:50:37 +0100

    fix assen and end year date sort bug

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -5,7 +5,7 @@ _LOGGER = logging.getLogger(__name__)
 
 API = "api"
 NAME = "afvalwijzer"
-VERSION = "2023.06.01"
+VERSION = "2023.12.01"
 ISSUE_URL = "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
 
 SENSOR_COLLECTOR_TO_URL = {
