f82591972e31b8b5300063de74fb878abdfe54cd
Bram van Dartel <root@rootrulez.com>
2024-01-14 21:19:45 +0100

    implement bumpver

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -5,7 +5,7 @@ _LOGGER = logging.getLogger(__name__)
 
 API = "api"
 NAME = "afvalwijzer"
-VERSION = "2024.12.12-beta.3"
+VERSION = "2024.01.06"
 ISSUE_URL = "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
 
 SENSOR_COLLECTOR_TO_URL = {
