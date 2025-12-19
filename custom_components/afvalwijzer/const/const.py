037413210267b097085d00814266a4125e1c93dd
Louis Matthijssen <louis@u5r.nl>
2025-12-19 06:21:50 +0100

    Fix duplicate waste for Reinis (#461)

--- custom_components/afvalwijzer/const/const.py
+++ custom_components/afvalwijzer/const/const.py
@@ -5,7 +5,7 @@ _LOGGER = logging.getLogger(__name__)
 
 API = "api"
 NAME = "afvalwijzer"
-VERSION = "2026.01.01"
+VERSION = "2025.12.01"
 
 ISSUE_URL = "https://github.com/xirixiz/homeassistant-afvalwijzer/issues"
 
