771a7fdbe5037907b6e2b80c53155fbdaed0bd48
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2026-01-10 06:56:09 +0000

    Remove unused shebang

--- custom_components/afvalwijzer/sensor_provider.py
+++ custom_components/afvalwijzer/sensor_provider.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from datetime import datetime, timedelta
 import hashlib
 
--- custom_components/afvalwijzer/tests/test_module.py
+++ custom_components/afvalwijzer/tests/test_module.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """Sensor component for AfvalWijzer
 Author: Bram van Dartel - xirixiz
 
