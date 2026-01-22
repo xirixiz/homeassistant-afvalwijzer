5348bdf3f6f8e65cc006ff1d0b1ebd779819ee08
Sebastiaan Speck <shem.speck@gmail.com>
2026-01-22 14:56:27 +0100

    Extend test_module with all missing providers

--- custom_components/afvalwijzer/tests/test_module.py
+++ custom_components/afvalwijzer/tests/test_module.py
@@ -27,10 +27,17 @@ default_label = "geen"
 providers = [
     {
         "provider": "acv",
-        "enabled": True,
+        "enabled": False,
         "addresses": [
             {"postal_code": "6713CG", "street_number": "11"},
             {"postal_code": "6714KK", "street_number": "20"},
+        ],
+    },
+    {
+        "provider": "afval3xbeter",
+        "enabled": False,
+        "addresses": [
+            {"postal_code": "4874AA", "street_number": "42"},
         ],
     },
     {
@@ -42,9 +49,16 @@ providers = [
     },
     {
         "provider": "afvalstoffendienst",
-        "enabled": False,
+        "enabled": True,
         "addresses": [
-            {"postal_code": "4266NB", "street_number": "1"},
+            {"postal_code": "5222AA", "street_number": "5"},
+        ],
+    },
+    {
+        "provider": "afvalstoffendienstkalender",
+        "enabled": True,
+        "addresses": [
+            {"postal_code": "5237BE", "street_number": "2"},
         ],
     },
     {
@@ -55,10 +69,32 @@ providers = [
         ],
     },
     {
+        "provider": "alphenaandenrijn",
+        "enabled": False,
+        "addresses": [
+            {"postal_code": "2408AV", "street_number": "3"},
+        ],
+    },
+    {
         "provider": "amsterdam",
         "enabled": False,
         "addresses": [
             {"postal_code": "1066KC", "street_number": "18"},
+            {"postal_code": "1013BG", "street_number": "1"},
+        ],
+    },
+    {
+        "provider": "areareiniging",
+        "enabled": False,
+        "addresses": [
+            {"postal_code": "7741AA", "street_number": "1"},
+        ],
+    },
+    {
+        "provider": "assen",
+        "enabled": False,
+        "addresses": [
+            {"postal_code": "9403AC", "street_number": "1"},
         ],
     },
     {
@@ -69,6 +105,30 @@ providers = [
         ],
     },
     {
+        "provider": "avri",
+        "enabled": False,
+        "addresses": [
+            {"postal_code": "4104BK", "street_number": "1"},
+        ],
+    },
+    {
+        "provider": "bar",
+        "enabled": False,
+        "addresses": [
+            {"postal_code": "3161AC", "street_number": "12"},
+        ],
+    },
+    {
+        "provider": "berkelland",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "blink",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
         "provider": "circulus",
         "enabled": False,
         "addresses": [
@@ -76,11 +136,42 @@ providers = [
         ],
     },
     {
+        "provider": "cranendonck",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "cyclus",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
         "provider": "dar",
         "enabled": False,
         "addresses": [
             {"postal_code": "6532AJ", "street_number": "1"},
+            {"postal_code": "6665CN", "street_number": "1"},
         ],
+    },
+    {
+        "provider": "deafvalapp",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "defryskemarren",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "denhaag",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "gad",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "geertruidenberg",
@@ -90,6 +181,16 @@ providers = [
         ],
     },
     {
+        "provider": "groningen",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "hellendoorn",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
         "provider": "hvc",
         "enabled": False,
         "addresses": [
@@ -97,11 +198,31 @@ providers = [
         ],
     },
     {
+        "provider": "irado",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "lingewaard",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
         "provider": "maassluis",
         "enabled": False,
         "addresses": [
             {"postal_code": "3146BL", "street_number": "22"},
         ],
+    },
+    {
+        "provider": "meerlanden",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "middelburg-vlissingen",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "mijnafvalwijzer",
@@ -115,7 +236,23 @@ providers = [
             {"postal_code": "3951EN", "street_number": "1"},
             {"postal_code": "3951EB", "street_number": "1"},
             {"postal_code": "3941RK", "street_number": "50", "suffix": "B"},
+            {"postal_code": "5508SB", "street_number": "51"},
         ],
+    },
+    {
+        "provider": "mijnafvalzaken",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "montferland",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "montfoort",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "nijkerk",
@@ -123,6 +260,11 @@ providers = [
         "addresses": [
             {"postal_code": "3861XJ", "street_number": "1"},
         ],
+    },
+    {
+        "provider": "offalkalinder",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "omrin",
@@ -135,12 +277,31 @@ providers = [
         ],
     },
     {
+        "provider": "oudeijsselstreek",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "peelenmaas",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
         "provider": "prezero",
         "enabled": False,
         "addresses": [
-            {"postal_code": "6665CN", "street_number": "1"},
             {"postal_code": "6822NG", "street_number": "1"},
         ],
+    },
+    {
+        "provider": "purmerend",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "rad",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "rd4",
@@ -148,6 +309,11 @@ providers = [
         "addresses": [
             {"postal_code": "6301ET", "street_number": "6"},
         ],
+    },
+    {
+        "provider": "recycleapp",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "reinis",
@@ -186,11 +352,52 @@ providers = [
         ],
     },
     {
+        "provider": "schouwen-duiveland",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "sliedrecht",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "spaarnelanden",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "straatbeeld",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "sudwestfryslan",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "suez",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
         "provider": "tilburg",
         "enabled": False,
         "addresses": [
             {"postal_code": "5038EC", "street_number": "37"},
+            {"postal_code": "5071KB", "street_number": "1"},
         ],
+    },
+    {
+        "provider": "twentemilieu",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "uithoorn",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "venlo",
@@ -200,11 +407,29 @@ providers = [
         ],
     },
     {
-        "provider": "veldhoven",
+        "provider": "venray",
         "enabled": False,
-        "addresses": [
-            {"postal_code": "5508SB", "street_number": "51"},
-        ],
+        "addresses": [],
+    },
+    {
+        "provider": "voorschoten",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "waalre",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "waardlanden",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "westland",
+        "enabled": False,
+        "addresses": [],
     },
     {
         "provider": "woerden",
@@ -213,10 +438,20 @@ providers = [
             {"postal_code": "3446GL", "street_number": "16"},
         ],
     },
+    {
+        "provider": "ximmio",
+        "enabled": False,
+        "addresses": [],
+    },
+    {
+        "provider": "zrd",
+        "enabled": False,
+        "addresses": [],
+    },
 ]
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 def _run_for_entry(entry: dict) -> None:
@@ -225,7 +460,7 @@ def _run_for_entry(entry: dict) -> None:
     street_number = entry.get("street_number")
     suffix = entry.get("suffix", "")
 
-    logger.info(
+    LOGGER.info(
         "\n--- Running provider: %s, postal_code: %s, street_number: %s ---",
         provider,
         postal_code,
@@ -243,24 +478,40 @@ def _run_for_entry(entry: dict) -> None:
         default_label,
     )
 
-    logger.info("Waste data with today: %s", collector.waste_data_with_today)
-    logger.info("Waste data without today: %s", collector.waste_data_without_today)
-    logger.info("Waste data custom: %s", collector.waste_data_custom)
-    logger.info("Waste types provider: %s", collector.waste_types_provider)
-    logger.info("Waste types custom: %s", collector.waste_types_custom)
-    logger.info("Waste notifications: %s", collector.notification_data)
+    if collector.waste_data_with_today == {} or collector.waste_types_provider == []:
+        LOGGER.error(
+            "Failed to fetch waste data for provider: %s, postal_code: %s, street_number: %s",
+            provider,
+            postal_code,
+            street_number,
+        )
+        return
+
+    LOGGER.info("Waste data with today: %s", collector.waste_data_with_today)
+    LOGGER.info("Waste data without today: %s", collector.waste_data_without_today)
+    LOGGER.info("Waste data custom: %s", collector.waste_data_custom)
+    LOGGER.info("Waste types provider: %s", collector.waste_types_provider)
+    LOGGER.info("Waste types custom: %s", collector.waste_types_custom)
+    LOGGER.info("Waste notifications: %s", collector.notification_count)
 
 
 if __name__ == "__main__":
     for prov in providers:
         provider_name = prov.get("provider")
         if not prov.get("enabled", True):
-            logger.info("Skipping disabled provider: %s", provider_name)
+            LOGGER.warning("Skipping disabled provider: %s", provider_name)
             continue
 
-        for addr in prov.get("addresses", []):
+        addresses = prov.get("addresses", [])
+        if addresses == []:
+            LOGGER.warning(
+                "Skipping provider, since no addresses defined: %s", provider_name
+            )
+            continue
+
+        for addr in addresses:
             entry = {"provider": provider_name, **addr}
             try:
                 _run_for_entry(entry)
             except Exception as exc:  # pragma: no cover - manual test runner
-                logger.exception("Error while running entry %s: %s", entry, exc)
+                LOGGER.exception("Error while running entry %s: %s", entry, exc)
