788c52d72f9fbffaa00b24cfbe50dab58ecc82e8
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2026-01-22 16:55:50 +0100

    Fix typo

--- custom_components/afvalwijzer/common/main_functions.py
+++ custom_components/afvalwijzer/common/main_functions.py
@@ -73,7 +73,7 @@ def waste_type_rename(item_name: str) -> str:
         "grijze container": "restafval",
         "kerstb": "kerstboom",
         "kerstboom": "kerstbomen",
-        "keukenafval": "vest-goed",
+        "keukenafval": "vet-goed",
         "opk": "papier",
         "oud papier en karton": "papier",
         "packages": "pmd",
