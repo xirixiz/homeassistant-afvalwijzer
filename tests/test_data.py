e7c0a95836f17cff09249f9655a9694a9a01f32d
Sebastiaan Speck <shem.speck@gmail.com>
2026-08-08 21:16:25 +0200

    fix(test): replace stale rd4 test address
    The rd4 API returns 422 "We kunnen geen adres vinden voor de ingevulde
    gegevens" for 6291VH/4, failing the daily run. House numbers 2 and 4 do
    not exist at that postal code; 6 does and returns a full schedule.
    
    The other 12 rd4 addresses were verified healthy.

--- tests/test_data.py
+++ tests/test_data.py
@@ -256,7 +256,7 @@ TEST_ADDRESSES = [
     #{"provider": "rd4", "postal_code": "6231CB", "house_number": "85"},
     #{"provider": "rd4", "postal_code": "6245TN", "house_number": "19"},
     #{"provider": "rd4", "postal_code": "6271EL", "house_number": "32"},
-    #{"provider": "rd4", "postal_code": "6291VH", "house_number": "4"},
+    #{"provider": "rd4", "postal_code": "6291VH", "house_number": "6"},
     #{"provider": "rd4", "postal_code": "6301ET", "house_number": "6"},
     #{"provider": "rd4", "postal_code": "6351AR", "house_number": "49"},
     #{"provider": "rd4", "postal_code": "6367XX", "house_number": "2"},
